e9416a8fb046f3dad194c768e04d51f89a90fa53
Marco Munteanu <marcomunteanu02@gmail.com>
2023-05-12 18:50:02 +0300

    modify tech section

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -12,8 +12,8 @@ function Technologies() {
         <div className='pfo__technologies-content_heading'>
           <h4>Never settle!</h4>
           <h1>Discover new <span className='gradient__text-orange'>technologies</span></h1>
-          <p>I am passionate about technology, innovation and big challenging tasks on my to do list. Never stop learning : currently I am improving my frontend skills by learning <span className='gradient__text-pink'>React.js</span> with
-          <span className='gradient__text-pink'> Node.js</span> on the backend coupled with <span className='gradient__text-pink'>Mongo DB</span>. This is so fun !</p>
+          <p>I am passionate about technology, innovation and big challenging tasks on my to do list. Never stop learning : currently I am improving my frontend skills by learning <span className='gradient__text-pink'>Angular</span> with
+          <span className='gradient__text-pink'> ASP.NET </span> on the backend.</p>
           <h4 className='gradient__text-pink'>Never settle!</h4>
         </div>
 
